989826a4cb541112ce86ca654c3cf0823c1eb8ea
PavanKumar380 <pavankumar@edufor.me>
2025-12-06 18:10:02 +0530

    Add 100 through 102

---
+++ 1 JavaScript/101 Lab 1 - Build a JavaScript Trivia Bot/script.js
@@ -0,0 +1,18 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "triviaBot", botLocation="Earth", favoriteLanguage="JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact="One fun fact about " + favoriteLanguage +  " is that it was created in 1995.";
+console.log(codingFact);
+
+codingFact="Another fun fact about " + favoriteLanguage +  " is that, as of 2025, 98.9% of websites use it on the client side.";
+console.log(codingFact);
+
+codingFact="Although Java and " + favoriteLanguage +  " are similar in name and syntax, the two langauges are distinct and differ greatly in design.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
